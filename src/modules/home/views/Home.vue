c4f73ff9ae5cadead51713cf440743700b123541
dcode <dr.glasdou@gmail.com>
2022-02-11 09:31:49 -0600

    done: v1.0.0

---
+++ src/modules/home/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="home">
+    <h1>home</h1>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'home-page',
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/main.scss';
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background: $primary;
+}
+</style>
